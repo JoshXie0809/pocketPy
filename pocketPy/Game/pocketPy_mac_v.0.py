f7d53b4591026495fac3a36801116a698df49ef7
JoshXie0809 <61863381+JoshXie0809@users.noreply.github.com>
2020-08-11 11:51:01 +0800

    mac

--- pocketPy/Game/pocketPy_mac_v.0.py
+++ pocketPy/Game/pocketPy_mac_v.0.py
@@ -1,22 +1,39 @@
 import pygame
 import sys
+import numpy as np
 
 path = '/Users/xiezhengqi/Desktop/GitHub/pocketPy/pocketPy/Game'
 
 pygame.init()
 screenSize = (480, 360)
-
+clock = pygame.time.Clock()
 pygame.display.set_caption('pocketPy mac v 0.0')
 screen = pygame.display.set_mode(screenSize)
+screen.fill((0, 0, 0))
+pygame.display.update()
 
-running = True
 
-while running:
+def start(event):
+    font = pygame.font.SysFont("comicsansms", 25)
+    text = font.render("Pressed Enter to Go", True, (255, 255, 255))
+    screen.blit(text, ((screenSize[0] - text.get_width()) // 2, 5/3*(screenSize[1] - text.get_height()) // 2))
+    pygame.display.update()
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K
+
+
+
+done = False
+running = False
+
+while not done:
     screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
             sys.exit()
-
     pygame.display.update()
+    start(event)
+
+
