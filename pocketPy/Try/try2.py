431f82aca4ee2aaa32b1a55486e44183610e22a3
JoshXie0809 <61863381+JoshXie0809@users.noreply.github.com>
2020-08-12 10:50:31 +0800

    mac

--- pocketPy/Try/try2.py
+++ pocketPy/Try/try2.py
@@ -1,4 +1,17 @@
 import pygame
 import os
 
-print(os.path.split(os.path.abspath(__file__)))
+mainDir = os.path.split(os.path.abspath(__file__))[0]
+## load Img
+
+
+
+
+## initiate
+
+pygame.init()
+pygame.font.init()
+
+
+pygame.font.quit()
+pygame.quit()
