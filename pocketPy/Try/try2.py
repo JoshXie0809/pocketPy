a1804c3f4966a41366efdd165553f0aa13e49bbc
JoshXie0809 <61863381+JoshXie0809@users.noreply.github.com>
2020-08-15 01:45:39 +0800

    none

--- pocketPy/Try/try2.py
+++ pocketPy/Try/try2.py
@@ -2,16 +2,22 @@ import pygame
 import os
 
 mainDir = os.path.split(os.path.abspath(__file__))[0]
-## load Img
+SCREENRECT = pygame.Rect(0, 0, 640, 480)
 
 
+# load Img
+def loadImg(filename):
+    try:
+        surface = pygame.image.load(os.path.join(mainDir, "Img", filename))
+    except pygame.error:
+        raise SystemExit('Could not load file')
+    return surface.convert()
 
 
-## initiate
+# initiate
 
 pygame.init()
 pygame.font.init()
-
 
 pygame.font.quit()
 pygame.quit()
